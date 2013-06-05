605b2c5423734a720375d1701d3efd35771a49b0
Xiaofeng Wang <xfun68@gmail.com>
2013-06-05 22:51:29 +0800

    Refactoring 5: Replace useless temp with query, here it is thisAmount.

--- src/Customer.java
+++ src/Customer.java
@@ -24,18 +24,15 @@ public class Customer {
         String result = "Rental Record for " + getName() + "\n";
 
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
             Rental each = rentals.nextElement();
-
-            thisAmount = each.getCharge();
 
             frequentRenterPoints++;
 
             if (each.getMovie().getPriceCode() == Movie.NEW_RELEASE && each.getDaysRented() > 1)
                 frequentRenterPoints++;
 
-            result += "\t" + each.getMovie().getTitle() + "\t" + thisAmount + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + each.getMovie().getTitle() + "\t" + each.getCharge() + "\n";
+            totalAmount += each.getCharge();
         }
 
         result += "Amount owed is " + totalAmount + "\n";

ed7d5d99a36f193aaf2dab730b08a229e3e9b08a
Xiaofeng Wang <xfun68@gmail.com>
2013-06-05 23:37:53 +0800

    Refactoring 11: Extract logic of calculating charge into Movie.

--- src/Movie.java
+++ src/Movie.java
@@ -22,4 +22,24 @@ public class Movie {
     public String getTitle() {
         return title;
     }
+
+    double getCharge(int daysRented) {
+        double result = 0;
+        switch (getPriceCode()) {
+            case REGULAR:
+                result += 2;
+                if (daysRented > 2)
+                    result += (daysRented - 2) * 1.5;
+                break;
+            case NEW_RELEASE:
+                result += daysRented * 3;
+                break;
+            case CHILDREN:
+                result += 1.5;
+                if (daysRented > 3)
+                    result += (daysRented - 3) * 1.5;
+                break;
+        }
+        return result;
+    }
 }
